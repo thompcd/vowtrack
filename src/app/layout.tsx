29b18b9594919946ab9f43aacd9f4869a316d9fe
Corey Thompson <corey.dwayne.thompson@gmail.com>
2025-08-24 12:31:45 +0000

    add vercel speed insights

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from 'next/font/google'
 import './globals.css'
 import Header from '@/components/Header'
 import { ThemeProvider } from '@/context/ThemeContext'
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 const inter = Inter({ subsets: ['latin'] })
 
