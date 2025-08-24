d4fc3725207ca40aef6b11626d481d73a12665e3
Corey Thompson <corey.dwayne.thompson@gmail.com>
2025-08-24 22:02:01 +0000

    disable char escaping in linter

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,6 +11,12 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    rules: {
+      // Allow unescaped apostrophes and quotes in text nodes
+      'react/no-unescaped-entities': 'off',
+    },
+  },
 ];
 
 export default eslintConfig;
